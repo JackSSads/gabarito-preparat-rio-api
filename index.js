09bca8812158cf1c987bdc42d9a0adc77fe684e6
JackSSads <contatojacksonsouza@hotmail.com>
2026-01-18 17:09:55 -0300

    tratando url front

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
 
 app.use(cors({
-    origin: [process.env.URL_FRONT || "http://localhost:8080"],
+    origin: [process.env.URL_FRONT ?? "http://localhost:8080"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
